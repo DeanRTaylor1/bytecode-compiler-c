d469791467feadb5176be44f158cef06244e32f4
deanrtaylor1 <deanrtaylor@hotmail.com>
2023-02-09 15:34:40 +0700

    end of 16.1

--- main.c
+++ main.c
@@ -3,44 +3,75 @@
 #include "debug.h"
 #include "vm.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int arg, const char* argv[]) {
+static void repl() {
+  char line[1024];
+  for (;;) {
+    printf("> ");
+
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+
+    interpret(line);
+  }
+}
+
+static char *readFile(const char *path) {
+  FILE *file = fopen(path, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "could not open file \"%s\".\n", path);
+    exit(74);
+  }
+
+  // fseek find the end of the file
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file); // How many bytes is the file
+  rewind(file); // move back to the start of the file after storing the required
+                // size
+
+  char *buffer =
+      (char *)malloc(fileSize + 1); // allocate enough memory for the file
+  if (buffer == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+    exit(74);
+  }
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize,
+                           file); // read the file to the buffer
+  if (bytesRead < fileSize) {
+    fprintf(stderr, "Could not read file \"%s\".\n");
+    exit(74);
+  }
+  buffer[bytesRead] = '\0';
+  return buffer;
+}
+
+static void runFile(const char *path) {
+  char *source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR)
+    exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR)
+    exit(70);
+}
+
+int main(int argc, const char *argv[]) {
   initVM();
 
-  Chunk chunk;
-  initChunk(&chunk);
-  
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path]\n");
+  }
 
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-   constant = addConstant(&chunk, 7.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123); /*writeChunk(&chunk, OP_NEGATE, 123);*/
-
-  constant = addConstant(&chunk, 56);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
-
-  disassembleChunk(&chunk, "text chunk");
-  interpret(&chunk);
   freeVM();
-  freeChunk(&chunk);
   return 0;
 }
--- vm.c
+++ vm.c
@@ -1,6 +1,7 @@
 #include "vm.h"
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "value.h"
 #include <stdio.h>
@@ -85,8 +86,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
